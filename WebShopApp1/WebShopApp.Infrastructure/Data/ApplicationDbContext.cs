d7b69404561687fea985628d6a65782bb19ba6f7
D1dko <deqnyordanov@mail.bg>
2023-12-04 11:51:55 +0200

    WebShopApp p1 is working

---
+++ WebShopApp1/WebShopApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using WebShopApp.Infrastructure.Data.Domain;
+
+namespace WebShopApp.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+            public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<Product> Product { get; set; }
+    }
+}
